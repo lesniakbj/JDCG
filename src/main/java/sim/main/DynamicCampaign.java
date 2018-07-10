9f2636469100fa6a799eeb60b25450f1681d8cbf
Brendan <brendan.lesniak@gmail.com>
2018-07-10 17:49:05 -0500

    Added Gradle to the workspace

---
+++ src/main/java/sim/main/DynamicCampaign.java
@@ -0,0 +1,22 @@
+package sim.main;
+
+public class DynamicCampaign {
+    private GlobalSimSettings simSettings;
+    private CampaignSettings campaignSettings;
+
+    public GlobalSimSettings getSimSettings() {
+        return simSettings;
+    }
+
+    public void setSimSettings(GlobalSimSettings simSettings) {
+        this.simSettings = simSettings;
+    }
+
+    public CampaignSettings getCampaignSettings() {
+        return campaignSettings;
+    }
+
+    public void setCampaignSettings(CampaignSettings campaignSettings) {
+        this.campaignSettings = campaignSettings;
+    }
+}
---
+++ src/main/java/sim/main/GlobalSimSettings.java
@@ -0,0 +1,9 @@
+package sim.main;
+
+public class GlobalSimSettings {
+    private int minutesPerSimulationStep;
+
+    public GlobalSimSettings() {
+        this.minutesPerSimulationStep = 10;
+    }
+}
