3e17d93da977004092582ef85333dea0e025ca85
Brendan <brendan.lesniak@gmail.com>
2018-07-14 21:33:26 -0500

    Cleaned imports

--- src/main/java/ui/containers/CampaignPanel.java
+++ src/main/java/ui/containers/CampaignPanel.java
@@ -30,10 +30,6 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static ui.util.ImageScaleUtil.MAP_IMAGE_HEIGHT_RATIO;
